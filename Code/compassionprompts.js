b43b786bfa0d54f9300e4d06441905988d4128b4
Seph <kayb93@gmail.com>
2026-02-25 12:04:10 -0600

    Bug Fix

--- Code/compassionprompts.js
+++ Code/compassionprompts.js
@@ -41,7 +41,7 @@ function rollCompassionPrompt() {
     const effortLootEnabled = document.getElementById('effortLootCheck').checked;
     const lootSource = effortLootEnabled ? itemData.effortloot : itemData.loot;
     
-    const BONUS_COMPASSION_NAME_RAW = 'Exodus 420';
+    const BONUS_COMPASSION_NAME_RAW = 'Amata 1003';
     const BONUS_ITEM_NAME = 'Medic Trait';
 
     function normalizeCompassionName(value) {
